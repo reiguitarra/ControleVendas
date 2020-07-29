ec5445769b685bd0d0d22c6b7bec10bcec2d918e
Reinaldo Avelino <reiguitarra@gmail.com>
2020-07-29 20:36:46 -0300

    Criado um servico para listar os vendedores, e criado contole e view manualmente

---
+++ ControleVendas/Services/SellerService.cs
@@ -0,0 +1,25 @@
+﻿using ControleVendas.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControleVendas.Models;
+
+namespace ControleVendas.Services
+{
+    public class SellerService
+    {
+        private readonly ControleVendasContext _context;
+
+        public SellerService(ControleVendasContext context)
+        {
+            _context = context;
+        }
+
+
+        public List<Vendedor> FindAll()
+        {
+            return _context.Saller.ToList();
+        }
+    }
+}
--- ControleVendas/Startup.cs
+++ ControleVendas/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using ControleVendas.Data;
+using ControleVendas.Services;
 
 namespace ControleVendas
 {
@@ -42,6 +43,9 @@ namespace ControleVendas
 
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
