6258548814cb55b3c6124203c9405d8050942098
Reinaldo Avelino <reiguitarra@gmail.com>
2020-07-29 09:23:07 -0300

    Realizado o Scafolld da classe deártamentos

---
+++ ControleVendas/Data/ControleVendasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ControleVendas.Models;
+
+namespace ControleVendas.Data
+{
+    public class ControleVendasContext : DbContext
+    {
+        public ControleVendasContext (DbContextOptions<ControleVendasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ControleVendas.Models.Department> Department { get; set; }
+    }
+}
--- ControleVendas/Startup.cs
+++ ControleVendas/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ControleVendas.Data;
 
 namespace ControleVendas
 {
@@ -33,6 +35,9 @@ namespace ControleVendas
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ControleVendasContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ControleVendasContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
